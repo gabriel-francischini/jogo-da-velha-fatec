096e9961150b44b7ebd2715cbbbef87ca9cfa165
ClauPD <Dd040492>
2019-12-05 16:26:13 -0200

    Marcando posicao

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-#sei laaaaaaaaaaaaaaaaaa
+tabuleiro = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
 
 def entrada_do_usuario():
 	return [0, 0]
 
-def fazer_jogada(posicao):
-	return None
+def fazer_jogada(posicao, tabuleiro):
+	
+	if posicao == [0,0]:
+		posicao = 0
+	if posicao == [0,1]:
+		posicao = 1
+	if posicao == [0,2]:
+		posicao = 2
+	if posicao == [1,0]:
+		posicao = 3
+	if posicao == [1,1]:
+		posicao = 4
+	if posicao == [1,2]:
+		posicao = 5
+	if posicao == [2,0]:
+		posicao = 6
+	if posicao == [2,1]:
+		posicao = 7
+	if posicao == [2,2]:
+		posicao = 8
+	tabuleiro[posicao] = "X"
+
+	return posicao
 
 def checar_se_ganhou():
 	return True
@@ -13,5 +34,5 @@ def checar_se_ganhou():
 fim_de_jogo = False
 while fim_de_jogo != True:
 	jogada = entrada_do_usuario()
-	fazer_jogada(jogada)
+	fazer_jogada(posicao, tabuleiro)
 	fim_de_jogo = checar_se_ganhou()
